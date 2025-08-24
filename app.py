97fccb8abdffd75ed92b8a9e2734e5833835a421
Nishan <nishansarker850@gmail.com>
2025-08-25 02:47:10 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = Flask(__name__)
+
+# Load credentials from environment variable
+creds_json = os.environ.get("GOOGLE_CREDS_JSON")
+creds_dict = json.loads(creds_json)
+
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(creds)
+sheet = client.open("MyFlaskSheet").sheet1
+
+
+@app.route("/")
+def index():
+    return render_template("form.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    class_name = request.form["class"]
+    roll = request.form["roll"]
+    num1 = request.form["num1"]
+    num2 = request.form["num2"]
+
+    # Append data as new row
+    sheet.append_row([name, class_name, roll, num1, num2])
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
